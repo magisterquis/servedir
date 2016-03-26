74ad147ce2863c8601ac582e1538b1c1722638d3
Stuart <kd5pbo@gmail.com>
2016-03-26 18:25:05 -0400

    Update ALL the things

--- README.md
+++ README.md
@@ -1,17 +1,61 @@
 servedir
 ========
 
-A very small httpd.
+A very small httpd which serves static files.
 
+This update breaks the previous interface.  On the other hand, I can't imagine
+anybody actually used the previous code (and it's not likely anybody will use
+this, either).
+
+Yeah, yeah, it's a wrapper around `http.FileServer`.
+
+Examples
+--------
+Serve files from the current directory which holds the TLS cert and key.  In
+practice, this is probably a lousy idea.
+```sh
+$ servedir
+2016/03/26 18:19:49 Serving files from .
+2016/03/26 18:19:49 Listening on [::]:4433 (tcp6) for HTTPS requests
+2016/03/26 18:19:49 Listening on 0.0.0.0:8080 (tcp4) for HTTP requests
+2016/03/26 18:19:49 Listening on [::]:8080 (tcp6) for HTTP requests
+2016/03/26 18:19:49 Listening on 0.0.0.0:4433 (tcp4) for HTTPS requests
 ```
-Usage of servedir:
-  -addr=":8080": [IP Address and] port on which to listen.
-  -dir="./": Directory to serve.
-  -ip4=false: Use IPv4 (default).
-  -ip6=false: Use IPv6.
+
+Serve files only on port 80, HTTP, from `/tmp`.  This probably is a pretty
+bad idea as well.
+```sh
+$ servedir -dir /tmp -nohttps -http 80
 ```
-Examples:
+
+Securely serve files from your personal htdocs directory.
+```sh
+$ servedir -dir ~/htdocs -nohttp
 ```
-./servedir -addr=127.0.0.1:4444 -dir=./xfer
-./servedir -dir=./
+
+Usage
+-----
+```
+Usage servedir [options]
+
+Serves files from the given directory.  Content-type will be automatically
+determined.
+
+Options:
+  -4	Don't listen on IPv4
+  -6	Don't listen on IPv6
+  -cert certificate
+    	HTTPS certificate (ignored if -nohttps is given) (default "cert.pem")
+  -dir directory
+    	Files in this directory will be served (default ".")
+  -http address
+    	HTTP [address and] port (default "8080")
+  -https address
+    	HTTPS listen [address and] port (default ":4433")
+  -key key
+    	HTTPS key (ignored if -nohttps is given) (default "key.pem")
+  -nohttp
+    	Don't handle HTTP requests
+  -nohttps
+    	Do not handle HTTPS requests
 ```
--- servedir.go
+++ servedir.go
@@ -1,6 +1,15 @@
 package main
 
+/*
+ * servedir.go
+ * Small program to serve http from a directory
+ * By J. Stuart McMurray
+ * Created 20140815
+ * Last Modified 20160326
+ */
+
 import (
+	"crypto/tls"
 	"flag"
 	"fmt"
 	"log"
@@ -8,47 +17,208 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"sync"
+
+	"github.com/gorilla/handlers"
 )
 
 func main() {
-	ip4flag := flag.Bool("ip4", false, "Use IPv4 (default).")
-	ip6flag := flag.Bool("ip6", false, "Use IPv6.")
-	addr := flag.String("addr", ":8080", "[IP Address and] port on "+
-		"which to listen.")
-	dir := flag.String("dir", "./", "Directory to serve.")
+	var (
+		no4 = flag.Bool(
+			"4",
+			false,
+			"Don't listen on IPv4",
+		)
+		no6 = flag.Bool(
+			"6",
+			false,
+			"Don't listen on IPv6",
+		)
+		httpAddr = flag.String(
+			"http",
+			"8080",
+			"HTTP [`address` and] port",
+		)
+		dir = flag.String(
+			"dir",
+			".",
+			"Files in this `directory` will be served",
+		)
+		cert = flag.String(
+			"cert",
+			"cert.pem",
+			"HTTPS `certificate` (ignored if -nohttps is given)",
+		)
+		key = flag.String(
+			"key",
+			"key.pem",
+			"HTTPS `key` (ignored if -nohttps is given)",
+		)
+		noHTTPS = flag.Bool(
+			"nohttps",
+			false,
+			"Do not handle HTTPS requests",
+		)
+		httpsAddr = flag.String(
+			"https",
+			":4433",
+			"HTTPS listen [`address` and] port",
+		)
+		noHTTP = flag.Bool(
+			"nohttp",
+			false,
+			"Don't handle HTTP requests",
+		)
+	)
+	flag.Usage = func() {
+		fmt.Fprintf(
+			os.Stderr,
+			`Usage %v [options]
+
+Serves files from the given directory.  Content-type will be automatically
+determined.
+
+Options:
+`,
+			os.Args[0],
+		)
+		flag.PrintDefaults()
+	}
 	flag.Parse()
 
-	/* Make sure argv[1] is a directory */
-	s, err := os.Stat(*dir)
+	/* Make sure we're listening on something */
+	if *noHTTP && *noHTTPS {
+		log.Fatalf("Both -nohttp and -nohttps can't be given")
+	}
+	if *no4 && *no6 {
+		log.Fatalf("Both -no4 and -no6 can't be given")
+	}
+
+	/* Make sure the served directory is a directory */
+	if err := verifyDir(*dir); nil != err {
+		log.Fatalf("Cannot serve files from %v: %v", *dir, err)
+	}
+	log.Printf("Serving files from %v", *dir)
+
+	/* Set up a handler for the directory */
+	http.Handle(
+		"/",
+		handlers.CombinedLoggingHandler(
+			os.Stderr,
+			http.FileServer(http.Dir(*dir)),
+		),
+	)
+
+	/* Fix up the addresses */
+	pa := parsePort(*httpAddr)
+	ta := parsePort(*httpsAddr)
+
+	/* WaitGroup to signal end of service */
+	wg := &sync.WaitGroup{}
+
+	wg.Add(4)
+	go serve("tcp4", pa, !(*noHTTP || *no4), wg)
+	go serve("tcp6", pa, !(*noHTTP || *no6), wg)
+	go serveTLS("tcp4", ta, !(*noHTTPS || *no4), *cert, *key, wg)
+	go serveTLS("tcp6", ta, !(*noHTTPS || *no4), *cert, *key, wg)
+	wg.Wait()
+	log.Fatalf("Done")
+}
+
+/* verifyDir returns nil iff dir is a directory. */
+func verifyDir(dir string) error {
+	s, err := os.Stat(dir)
 	if err != nil {
-		log.Printf("Unable to stat %v: %v", *dir, err)
-		os.Exit(-3)
+		return err
 	}
 	if !s.IsDir() {
-		log.Printf("%v is not a directory", *dir)
-		os.Exit(-2)
+		return fmt.Errorf("%v is not a directory", dir)
 	}
-	/* Work out the port */
-	if _, err := strconv.Atoi(*addr); nil == err {
-		*addr = fmt.Sprintf(":%v", *addr)
+	return nil
+}
+
+/* parseAddress turns a port-only address into a :port address.  If the address
+isn't just a number, p is returned */
+func parsePort(p string) string {
+	if i, err := strconv.Atoi(p); nil == err {
+		return fmt.Sprintf(":%v", i)
 	}
-	/* Work out the network on which to listen */
-	network := "tcp4"
-	if *ip4flag && *ip6flag {
-		network = "tcp"
-	} else if *ip6flag {
-		network = "tcp6"
+	return p
+}
+
+/* serve serves up HTTP on proto and addr if ok is true.  It calls wg's Done
+method before returning. */
+func serve(proto, addr string, ok bool, wg *sync.WaitGroup) {
+	defer wg.Done()
+	if !ok {
+		return
 	}
-	/* Make a listener */
-	ln, err := net.Listen(network, *addr)
-	if err != nil {
-		log.Printf("Unable to listen: %v", err)
-		os.Exit(-1)
+	/* Listen on the address */
+	l, err := net.Listen(proto, addr)
+	if nil != err {
+		log.Printf(
+			"Unable to listen on %v (%v): %v",
+			addr,
+			proto,
+			err,
+		)
+		return
 	}
-	/* Make a server */
-	server := &http.Server{Addr: *addr,
-		Handler: http.FileServer(http.Dir(*dir))}
-	log.Printf("Serving up %v on %v\n", *dir, ln.Addr())
-	/* Start serving */
-	log.Fatal(server.Serve(ln))
+	log.Printf("Listening on %v (%v) for HTTP requests", l.Addr(), proto)
+	/* Serve */
+	log.Printf(
+		"Error serving HTTP requests on %v (%v): %v",
+		l.Addr(),
+		proto,
+		http.Serve(l, nil),
+	)
+}
+
+/* serveTLS serves up HTTPS requests on proto and addr using cert and key if ok
+is true.  It calls wg's Done method before returning. */
+func serveTLS(
+	proto, addr string,
+	ok bool,
+	cert, key string,
+	wg *sync.WaitGroup,
+) {
+	defer wg.Done()
+
+	/* Read cert and key */
+	c, err := tls.LoadX509KeyPair(cert, key)
+	if nil != err {
+		log.Printf(
+			"Unable to load TLS key from %v and %v for service "+
+				"on %v (%v): %v",
+			cert, key,
+			addr, proto,
+			err,
+		)
+		return
+	}
+
+	/* TLS config */
+	conf := &tls.Config{
+		Certificates: []tls.Certificate{c},
+	}
+
+	/* TLS listener */
+	l, err := tls.Listen(proto, addr, conf)
+	if nil != err {
+		log.Printf("Unable to listen on %v (%v) for HTTPS "+
+			"requests: %v",
+			addr, proto,
+			err,
+		)
+		return
+	}
+	log.Printf("Listening on %v (%v) for HTTPS requests", l.Addr(), proto)
+
+	/* Serve */
+	log.Printf(
+		"Error serving HTTP requests on %v (%v): %v",
+		l.Addr(),
+		proto,
+		http.Serve(l, nil),
+	)
 }
